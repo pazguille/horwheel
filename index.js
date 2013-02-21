97359a5ee36622c17fcf284b4549cfc6125215d4
Guille Paz <guille87paz@gmail.com>
2013-02-21 09:40:08 -0300

    Create version 0.0.1

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Privates
+ */
+var bind = (window.addEventListener !== undefined) ? 'addEventListener' : 'attachEvent',
+    wheel = (window.onwheel !== undefined) ? 'wheel' :
+                (window.attachEvent) ? 'onmousewheel' :
+                    (window.onmousewheel !== undefined) ? 'mousewheel' : 'DOMMouseScroll';
+
+function horizontalWheel(eve) {
+    eve = eve || window.eve;
+    eve.preventDefault();
+    // JSlint: Strict violation. Because 'this' in an anonymous function is undefined, but this function will receive an DOM element as context
+    this.scrollLeft += (eve.deltaY !== undefined) ? eve.deltaY :
+                        (eve.detail !== undefined && eve.detail !== 0) ? eve.detail :
+                            -eve.wheelDelta;
+}
+
+/**
+ * horwheel
+ */
+function horwheel(el) {
+    if (el === undefined) {
+        return;
+    }
+    el.style.overflow = 'auto';
+    el[bind](wheel, horizontalWheel, false);
+}
+
+/**
+ * Expose horwheel
+ */
+exports = module.exports = horwheel;
